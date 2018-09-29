0cffed8d49695ed37ed4b05b6f67a38563f06bf2
Girish Patwardhan <reach.to.girish@gmail.com>
2018-09-29 17:12:28 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+//  OpenShift sample Node application
+var express = require('express'),
+    app     = express(),
+    morgan  = require('morgan');
+app.engine('html', require('ejs').renderFile);
+app.use(morgan('combined'))
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+
+app.get('/', function (req, res) {
+res.render('index.html', { pageCountMessage : null});
+});
+
+app.use(require('body-parser').json());
+
+app.use(require('express-static')('./'));
+
+// error handling
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500).send('Something bad happened!');
+});
+
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+module.exports = app ;
